01b082ad663dc6a544d849d8d7c56d0153d6a7bb
Jonathan Hon <jonathanhon@hotmail.com>
2017-04-10 22:49:12 +0100

    tidied up the code

--- examples/advanced/auction_listener.py
+++ examples/advanced/auction_listener.py
@@ -1,11 +1,12 @@
 import os
 import tempfile
+
 from colony.node import Graph, DictionaryNode
 
 
 def get_auctions(category):
     if category == 'donkeys':
-        return {0:{'status':'OPEN'},
+        return {0: {'status': 'OPEN'},
                 1: {'status': 'OPEN'},
                 2: {'status': 'CLOSED'}}
     elif category == 'mules':
@@ -20,20 +21,20 @@ def delete(data):
     return 'delete', tuple(data.keys())
 
 
-def get_prices(auction_ids=None):
+def get_prices(auction_catalogue=None):
     results = {}
-    for auction_id in auction_ids:
-        if auction_id==2:
+    for auction_id in auction_catalogue:
+        if auction_id == 2:
             results[auction_id] = {'status': 'CLOSED', 'price': 123}
         else:
             results[auction_id] = {'status': 'OPEN', 'price': 123}
     return results
 
 
-def save_prices(prices):
-
+def save_prices(prices, auction_catalogue=None):
+    print('save_prices received auction catalogue of %s' % str(auction_catalogue))
     for k, v in prices.items():
-        print('Saving prices for %s: %s'%(str(k), str(v)))
+        print('Saving prices for %s: %s' % (str(k), str(v)))
 
     # Return the input. This is a bit smelly.
     return prices
@@ -42,7 +43,6 @@ def save_prices(prices):
 def close_filter(prices):
     result = {}
     for k, v in prices.items():
-        print(k, v)
         if v['status'] == 'CLOSED':
             result[k] = v
     return result
@@ -53,13 +53,16 @@ if __name__ == '__main__':
 
     get_auctions_node = graph.add_node(get_auctions)
 
-    update_node = graph.add_node(update, node_args=(get_auctions_node, ))
+    update_node = graph.add_node(update, node_args=(get_auctions_node,))
 
     name = os.path.basename(tempfile.NamedTemporaryFile().name)
-    active_auctions_node = graph.add(DictionaryNode, node_args=(update_node, ), name=name)
+    active_auctions_node = graph.add(DictionaryNode, node_args=(update_node,), name=name)
 
-    get_prices_node = graph.add_node(get_prices, node_kwargs={'auction_ids': active_auctions_node})
-    save_prices_node = graph.add_node(save_prices, node_args=(get_prices_node, ))
+    get_prices_node = graph.add_node(get_prices,
+                                     node_kwargs={'auction_catalogue': active_auctions_node})
+    save_prices_node = graph.add_node(save_prices,
+                                      node_args=(get_prices_node,),
+                                      node_kwargs={'auction_catalogue': active_auctions_node})
     close_filter_node = graph.add_node(close_filter, node_args=(save_prices_node,))
     delete_node = graph.add_node(delete, node_args=(close_filter_node,))
 
@@ -75,3 +78,6 @@ if __name__ == '__main__':
     print('prices %s' % get_prices_node.get_value())
     print('close_filter %s' % close_filter_node.get_value())
     print('active auctions %s' % active_auctions_node.get_value())
+
+    # get_auctions_node.notify('donkeys')
+    # print('prices %s' % get_prices_node.get_value())
