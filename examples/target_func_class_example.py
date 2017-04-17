152a9ab3c911f50623cca04f0e68e8e0c27cdf6d
Jonathan Hon <jonathanhon@hotmail.com>
2017-04-17 09:41:21 +0100

    Added target class example

--- examples/target_func_class_example.py
+++ examples/target_func_class_example.py
@@ -1,10 +1,9 @@
-from colony.node import Graph, Node
-from colony.observer import RememberingObserver
+from colony.node import Node
 
 
 class ExampleTargetClass(object):
     def __init__(self, c):
-        print('Initialising target_funcClass')
+        print('Initialising ExampleTargetClass')
         self.data = set([c])
         print('data is %s' % str(self.data))
 
@@ -13,16 +12,18 @@ class ExampleTargetClass(object):
         return self.data
 
 
-if __name__=='__main__':
-
-    n = Node(target_class=target_funcClass, target_class_args=(0,))
+if __name__ == '__main__':
+    n = Node(target_class=ExampleTargetClass, target_class_args=(0,))
     n.start()
 
     n.notify(1)
-    self.assertEqual({0, 1}, n.get_value())
+    # Expect {0, 1}
+    print(n.get_value())
 
     n.notify(2)
-    self.assertEqual({0, 1, 2}, n.get_value())
+    # Expect {0, 1, 2}
+    print(n.get_value())
 
     n.notify(3)
-    self.assertEqual({0, 1, 2, 3}, n.get_value())
+    # Expect {0, 1, 2, 3}
+    print(n.get_value())
